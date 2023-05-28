e87bd01695744181f940b27e2ceaaf4ec417e8ca
itsamankrsingh <itsamankrsingh@gmail.com>
2023-05-28 13:43:01 +0530

    First Example

---
+++ DelegateBasicsExample/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DelegateBasicsExample
+{
+    class Program
+    {
+        delegate void LogDel(string text);
+        static void LogTextToConsole(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+
+        static void Main(string[] args)
+        {
+            LogDel logDel = new LogDel(LogTextToConsole);
+            Console.WriteLine("Please enter your name: ");
+            var name = Console.ReadLine();
+            logDel(name);
+            Console.ReadKey();
+        }
+    }
+}
